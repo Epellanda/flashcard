860caa3ba36319dfb7d34a453a68f3012c40ccbb
Epellanda <eduarda.pellanda.rodrigues@escola.pr.gov.br>
2025-06-13 08:19:14 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'Biologia',
+    'Como ocorre a digestão de carboidratos, proteínas e lipídios no sistema digestório?',
+    'Carboidratos começam a ser digeridos na boca pela amilase salivar. Proteínas são quebradas no estômago pela pepsina. Lipídios são emulsificados pela bile e digeridos por lipases no intestino delgado, onde ocorre a absorção.
+
+'
+)
+
+criaCartao(
+    'Cabelo',
+    'O que motivou o uso de perucas na Europa entre os séculos XVII e XVIII?',
+    'As perucas eram símbolo de status, higiene (devido à falta de banho frequente), e moda aristocrática. Também escondiam calvície ou doenças como sífilis, comuns na época'
+)
+
+criaCartao(
+    'Animais',
+    'Qual é o animal mais rápido do mundo e como ele atinge essa velocidade?',
+    ' O falcão-peregrino. Em mergulho, pode atingir mais de 300 km/h graças à aerodinâmica de seu corpo e força muscular adaptada à caça em alta velocidade.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'How do you ask for directions in English?',
+    'You can say: “Excuse me, how do I get to the train station?”'
+)
